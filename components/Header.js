1d82486b409fe69190ead87950a2f0562fc80dea
JunkPixel <ricardotomaz027@gmail.com>
2021-05-10 11:20:58 +0100

    Login & get JWT

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,11 @@ import Link from "next/link";
 import styles from "@/styles/Header.module.css";
 import Search from "./Search";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import AuthContext from "@/context//AuthContext";
+import { useContext } from "react";
+
 const Header = () => {
+  const { user, logout } = useContext(AuthContext);
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -18,19 +22,39 @@ const Header = () => {
               <a>Events</a>
             </Link>
           </li>
-          <li>
-            <Link href="/events/add">
-              <a>Add Event</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/account/login">
-              <a className="btn-secondary btn-icon">
-                {" "}
-                <FaSignInAlt /> Log in
-              </a>
-            </Link>
-          </li>
+          {user ? (
+            // if logged im
+            <>
+              <li>
+                <Link href="/events/add">
+                  <a>Add Event</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/account/dashboard">
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={() => logout()}
+                  className="btn-secondary btn-icon"
+                >
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            // if logged out
+            <li>
+              <Link href="/account/login">
+                <a className="btn-secondary btn-icon">
+                  {" "}
+                  <FaSignInAlt /> Log in
+                </a>
+              </Link>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
